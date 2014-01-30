45534d9080a34a3acea2b012cd9daec75a458f4c
root <root@Raspbian.(none)>
2014-01-30 20:54:31 +0000

    Comment cronfile.txt

--- wifi_control.sh
+++ wifi_control.sh
@@ -4,7 +4,7 @@ MY_APP_ID="RaspHomCenter.app"
 MY_APP_TOKEN="GhTmv6Mgaid4r+iZWnO+7Ox/qEfFZOIkLf1TlT9TD0GdmmHz8nK/uXz87HvCpv+j"
 
 # source the freeboxos-bash-api
-source /domo/freebox/freeboxos_bash_api.sh
+source ./freeboxos_bash_api.sh
 
 # login
 login_freebox "$MY_APP_ID" "$MY_APP_TOKEN"
